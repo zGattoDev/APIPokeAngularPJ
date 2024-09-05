986ccc468744e043896851f4bcdc885f39be1a66
SrGatoMcTaco <eduardo19910@hotmail.com>
2024-09-05 12:50:10 -0500

    prueba de pull

--- src/app/pokeapi.service.spec.ts
+++ src/app/pokeapi.service.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed } from '@angular/core/testing';
+/*import { TestBed } from '@angular/core/testing';
 
 import { PokeapiService } from './pokeapi.service';
 
@@ -15,3 +15,4 @@ describe('PokeapiService', () => {
     console.log('La prueba "should be created" ha pasado correctamente.');
   });
 });
+*/
--- src/app/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -6,7 +6,7 @@ import { PokemonDialogComponent } from '../pokemon-dialog/pokemon-dialog.compone
 @Component({
   selector: 'app-pokemon-list',
   templateUrl: './pokemon-list.component.html',
-  styleUrls: ['./pokemon-list.component.css']
+  styleUrls: ['./pokemon-list.component.css'],
 })
 export class PokemonListComponent implements OnInit {
   pokemonList: any[] = [];
@@ -17,24 +17,26 @@ export class PokemonListComponent implements OnInit {
   constructor(
     private pokeapiService: PokeapiService,
     public dialog: MatDialog
-  ) { }
+  ) {}
 
   ngOnInit(): void {
     this.loadPokemonList();
   }
 
   loadPokemonList(): void {
-    this.pokeapiService.getPokemonList(this.currentPage).subscribe((data: any) => {
-      this.pokemonList = data.results;
-      this.pokemonList.forEach((pokemon: any) => {
-        this.getPokemonDetails(pokemon.name);
+    this.pokeapiService
+      .getPokemonList(this.currentPage)
+      .subscribe((data: any) => {
+        this.pokemonList = data.results;
+        this.pokemonList.forEach((pokemon: any) => {
+          this.getPokemonDetails(pokemon.name);
+        });
       });
-    });
   }
 
   getPokemonDetails(name: string): void {
     this.pokeapiService.getPokemonDetails(name).subscribe((data: any) => {
-      const index = this.pokemonList.findIndex(p => p.name === name);
+      const index = this.pokemonList.findIndex((p) => p.name === name);
       if (index !== -1) {
         this.pokemonList[index] = data;
       }
@@ -62,35 +64,35 @@ export class PokemonListComponent implements OnInit {
       case 'water':
         return '#85a8fb';
       case 'rock':
-        return '#8b3e22'
+        return '#8b3e22';
       case 'bug':
-        return '#3c9950'
+        return '#3c9950';
       case 'dark':
-        return '#595978'
+        return '#595978';
       case 'dragon':
-        return '#62cad9'
+        return '#62cad9';
       case 'electric':
-        return '#fbfa72'
+        return '#fbfa72';
       case 'fairy':
-        return '#ea1368'
+        return '#ea1368';
       case 'fighting':
-        return '#f06239'
+        return '#f06239';
       case 'flying':
-        return '#94b2c7'
+        return '#94b2c7';
       case 'ghost':
-        return '#906791'
+        return '#906791';
       case 'ground':
-        return '#6e491f'
+        return '#6e491f';
       case 'ice':
-        return '#d8f0fa'
+        return '#d8f0fa';
       case 'normal':
-        return '#ca98a6'
+        return '#ca98a6';
       case 'poison':
-        return '#9b69da'
+        return '#9b69da';
       case 'psychic':
-        return '#f71d92'
+        return '#f71d92';
       case 'steel':
-        return '#43bd94'
+        return '#43bd94';
 
       default:
         return 'gray';
@@ -99,51 +101,72 @@ export class PokemonListComponent implements OnInit {
 
   getTypeNameInSpanish(typeName: string): string {
     switch (typeName) {
-      case 'normal': return 'Normal';
-      case 'fire': return 'Fuego';
-      case 'fighting': return 'Lucha';
-      case 'water': return 'Agua';
-      case 'flying': return 'Volador';
-      case 'grass': return 'Planta';
-      case 'poison': return 'Veneno';
-      case 'electric': return 'Eléctrico';
-      case 'ground': return 'Tierra';
-      case 'psychic': return 'Psíquico';
-      case 'rock': return 'Roca';
-      case 'ice': return 'Hielo';
-      case 'bug': return 'Bicho';
-      case 'dragon': return 'Dragón';
-      case 'ghost': return 'Fantasma';
-      case 'dark': return 'Siniestro';
-      case 'steel': return 'Acero';
-      case 'fairy': return 'Hada';
-      default: return 'Desconocido';
+      case 'normal':
+        return 'Normal';
+      case 'fire':
+        return 'Fuego';
+      case 'fighting':
+        return 'Lucha';
+      case 'water':
+        return 'Agua';
+      case 'flying':
+        return 'Volador';
+      case 'grass':
+        return 'Planta';
+      case 'poison':
+        return 'Veneno';
+      case 'electric':
+        return 'Eléctrico';
+      case 'ground':
+        return 'Tierra';
+      case 'psychic':
+        return 'Psíquico';
+      case 'rock':
+        return 'Roca';
+      case 'ice':
+        return 'Hielo';
+      case 'bug':
+        return 'Bicho';
+      case 'dragon':
+        return 'Dragón';
+      case 'ghost':
+        return 'Fantasma';
+      case 'dark':
+        return 'Siniestro';
+      case 'steel':
+        return 'Acero';
+      case 'fairy':
+        return 'Hada';
+      default:
+        return 'Desconocido';
     }
   }
 
   search(): void {
     if (this.searchTerm.trim() !== '') {
-      this.pokeapiService.getPokemonDetails(this.searchTerm.toLowerCase()).subscribe((data: any) => {
-        this.pokemonList = [data];
-      }, (error) => {
-        console.error('Error:', error);
-       
-      });
+      this.pokeapiService
+        .getPokemonDetails(this.searchTerm.toLowerCase())
+        .subscribe(
+          (data: any) => {
+            this.pokemonList = [data];
+          },
+          (error) => {
+            console.error('Error:', error);
+          }
+        );
     } else {
       this.loadPokemonList();
     }
   }
 
   openDialog(pokemon: any): void {
-    
     const dialogRef = this.dialog.open(PokemonDialogComponent, {
-      width: '250px',
-      data: { selectedPokemon: pokemon }
+      width: '60%px',
+      data: { selectedPokemon: pokemon },
     });
 
-    dialogRef.afterClosed().subscribe(result => {
+    dialogRef.afterClosed().subscribe((result) => {
       console.log('The dialog was closed');
     });
   }
 }
-
